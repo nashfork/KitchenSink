33d6259a407e1bc363bfefd5aabb84d6f1c4aa3e
Brendon George <brendongeorge@Brendons-MacBook-Air.local>
2020-02-06 14:29:25 -0700

    finish resume

--- Resume/app.js
+++ Resume/app.js
@@ -14,3 +14,38 @@ displayPosition(allcaps, resume.Career, resume.Description)
 
 console.log('My Interests:')
 displayPosition("* Fishing", "* Reading", "* Cross-stitch")
+
+console.log('My Previous Experience:')
+displayPosition('* History, Geography, Sociology', '* Undergraduate teaching History Department', '* Secretary at St. Pauls Lutheran Church')
+
+let act = [
+{
+name: 'fishing',
+quality: 'cool'
+},
+{
+name: 'writing',
+quality: 'not cool'
+},
+{
+name: 'walking',
+quality: 'not cool'
+},
+{
+name: 'parenting',
+quality: 'cool'
+}
+];
+function skillName(x) {
+    if (x.quality === 'cool') {
+        return console.log('* BAM:',x.name)
+    } else {
+        return console.log('*',x.name)
+    }
+}
+
+console.log('My Skills:')
+for (let i = 0; i < act.length; i++) {
+    skillName(act[i])
+}
+
